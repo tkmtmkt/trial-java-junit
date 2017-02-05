f73efcbde67342babd53f07abae564e1769746e2
Takamatsu Makoto <tkmtmkt@gmail.com>
2017-02-05 18:52:34 +0900

    add: sample source

---
+++ src/main/java/com/github/tkmtmkt/Main.java
@@ -0,0 +1,13 @@
+package com.github.tkmtmkt;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main
+{
+    static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        logger.info("Hello World");
+    }
+}
